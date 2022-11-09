d9d414283dfae32f80b57c986fda4b19e291aa35
ikamrava <ikamrava>
2022-11-09 21:41:56 +0000

    mobile-test

--- index.js
+++ index.js
@@ -29,10 +29,15 @@ const posts = [
   },
 ];
 
+heart = document.getElementById("heart");
+dm = document.getElementById("dm");
+com = document.getElementById("com");
+
 renderHtml = document.getElementById("rendering");
+renderlikes = document.getElementById("render-likes");
 let body = "";
 finalBody = "";
-
+secondpart = "";
 function rendering(list) {
   for (let i = 0; i < list.length; i++) {
     body = `
@@ -47,22 +52,29 @@ function rendering(list) {
     </div>
 </section>
 <img id = "main-img" src="${list[i].post}" alt="">
-<footer>
-    <div class="icons">
-        <img src="images/icon-heart.png" alt="">
-        <img src="images/icon-comment.png" alt="">
-        <img src="images/icon-dm.png" alt="">
-    </div>
-    <div class="comment-like">
-        <p id = "likes">${list[i].likes} likes</p>
-        <p id = "comments"><span>gus1819 &nbsp</span>${list[i].comment}</p>
+<div class="icons">
+          <img id="heart" src="images/icon-heart.png" alt="" />
+          <img id="com" src="images/icon-comment.png" alt="" />
+          <img id="dm" src="images/icon-dm.png" alt="" />
         </div>
-    </footer>`;
+<footer>
+    <div class="comment-like">
+    <p id = "likes-${i}">${list[i].likes} likes</p>
+    <p id = "comments"><span>gus1819 &nbsp</span>${list[i].comment}</p>
+    </div>
+    </footer>
+  `;
 
-    finalBody += body;
+    ``;
+
+    finalBody += body + secondpart;
   }
 
   return finalBody;
 }
 
 renderHtml.innerHTML = rendering(posts);
+
+heart.addEventListener("click", function () {
+  console.log("clicked");
+});
